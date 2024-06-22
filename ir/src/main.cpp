74abfda4d6387fc2d16c82a462f7629224824e92
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2024-06-22 21:41:14 +0900

    update

--- ir/src/main.cpp
+++ ir/src/main.cpp
@@ -13,10 +13,14 @@
 
 #include <ssid.h>
 
+#define UINT64_STR_LEN 16
+
 void handleRootAPI(void);
 void handleNotFoundAPI(void);
+void handleIRSendAPI(void);
 void handleIRDecodeAPI(void);
 
+#define BUTTON_PIN GPIO_NUM_3
 #define LED_PIN GPIO_NUM_2
 #define IR_SEND_PIN GPIO_NUM_5
 #define IR_RECV_PIN GPIO_NUM_4
@@ -24,6 +28,7 @@ void handleIRDecodeAPI(void);
 #define COLOR_SETUP pixels.Color(0, 32, 0)
 #define COLOR_NORMAL pixels.Color(0, 0, 32)
 #define COLOR_RECEIVE pixels.Color(32, 32, 0)
+#define COLOR_SEND pixels.Color(0, 32, 32)
 
 #define RECEIVING_MAX_WAIT_MS 5000
 
@@ -57,6 +62,7 @@ void setupWiFi()
 void setupIR()
 {
     irsend.begin();
+
     irrecv.setUnknownThreshold(kMinUnknownSize);
     irrecv.setTolerance(kTolerancePercentage);
     irrecv.enableIRIn();
@@ -67,8 +73,14 @@ void setupServer()
     // Webサーバの開始
     server.on("/", HTTP_GET, handleRootAPI);
     server.on("/ir/receive", HTTP_GET, handleIRDecodeAPI);
+    server.on("/ir/send", HTTP_POST, handleIRSendAPI);
     server.onNotFound(handleNotFoundAPI);
     server.begin();
+}
+
+void setupButton()
+{
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
 }
 
 void setup()
@@ -84,6 +96,7 @@ void setup()
     setupWiFi();
     setupIR();
     setupServer();
+    setupButton();
 
     pixels.setPixelColor(0, COLOR_NORMAL);
     pixels.show();
@@ -99,6 +112,147 @@ void handleRootAPI(void)
 void handleNotFoundAPI(void)
 {
     server.send(404, "application/json", "{\"message\": \"Not Found.\"}");
+}
+
+void writeJSONResponse(const char *api, int statusCode, JsonDocument &doc)
+{
+    char bodyBuf[1024 * 2];
+    size_t bodySize = serializeJson(doc, bodyBuf, sizeof(bodyBuf));
+    server.send(statusCode, "application/json", bodyBuf);
+
+    Serial.printf("response_body: %s\r\n", bodyBuf);
+    Serial.printf("POST /ir/send %d\r\n", statusCode);
+    pixels.setPixelColor(0, COLOR_NORMAL);
+    pixels.show();
+}
+
+uint64_t hexStringToUint64(const char *hexString)
+{
+    if (hexString[0] == '0' && (hexString[1] == 'x' || hexString[1] == 'X'))
+    {
+        hexString += 2;
+    }
+
+    uint64_t result = strtoull(hexString, NULL, 16);
+
+    return result;
+}
+
+// 16進数文字を対応する数値に変換
+unsigned char hexCharToValue(char hexChar)
+{
+    if ('0' <= hexChar && hexChar <= '9')
+    {
+        return hexChar - '0';
+    }
+    else if ('a' <= hexChar && hexChar <= 'f')
+    {
+        return hexChar - 'a' + 10;
+    }
+    else if ('A' <= hexChar && hexChar <= 'F')
+    {
+        return hexChar - 'A' + 10;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+// HEX文字列をバイト列に変換する関数
+void hexStringToByteArray(const char *hexString, unsigned char *byteArray, size_t *byteArraySize)
+{
+    if (hexString[0] == '0' && (hexString[1] == 'x' || hexString[1] == 'X'))
+    {
+        hexString += 2;
+    }
+
+    size_t hexLen = strlen(hexString);
+    *byteArraySize = hexLen / 2;
+
+    for (size_t i = 0; i < hexLen / 2; ++i)
+    {
+        byteArray[i] = (hexCharToValue(hexString[2 * i]) << 4) | hexCharToValue(hexString[2 * i + 1]);
+    }
+}
+
+void handleIRSendAPI(void)
+{
+    Serial.println("POST /ir/send");
+    pixels.setPixelColor(0, COLOR_SEND);
+    pixels.show();
+
+    JsonDocument resDoc;
+    JsonDocument reqDoc;
+
+    uint32_t now = millis();
+    resDoc["ts"] = now;
+    resDoc["lib_version"] = _IRREMOTEESP8266_VERSION_STR;
+    resDoc["success"] = false;
+
+    String requestBody = server.arg("plain");
+    Serial.printf("request body: %s\r\n", requestBody.c_str());
+    DeserializationError error = deserializeJson(reqDoc, requestBody);
+    if (error)
+    {
+        resDoc["error"] = "failed to deserialize Json.";
+        writeJSONResponse("POST /ir/send", 401, resDoc);
+        return;
+    }
+
+    uint64_t data[32];
+    const char *hexData = reqDoc["hex"];
+    if (hexData == NULL || strlen(hexData) == 0)
+    {
+        resDoc["error"] = "hex is required.";
+        writeJSONResponse("POST /ir/send", 401, resDoc);
+        return;
+    }
+
+    size_t dataSize;
+
+    const char *type = reqDoc["type"];
+    if (type == NULL || strlen(type) == 0)
+    {
+        resDoc["error"] = "type is required.";
+        writeJSONResponse("POST /ir/send", 401, resDoc);
+        return;
+    }
+
+    Serial.printf("type: %s, hex: %s\r\n", type, hexData);
+
+    if (strcmp(type, "SONY") == 0)
+    {
+        uint64_t data = hexStringToUint64(hexData);
+        Serial.printf("data: %llx\r\n", data);
+        irsend.sendSony(data, 12, 2);
+    }
+    else if (strcmp(type, "MITSUBISHI_AC") == 0)
+    {
+        if (strlen(hexData) > 32 * UINT64_STR_LEN)
+        {
+            resDoc["error"] = "hex is too long.";
+            writeJSONResponse("POST /ir/send", 401, resDoc);
+            return;
+        }
+
+        unsigned char data[64];
+        size_t size;
+        hexStringToByteArray(hexData, data, &size);
+        Serial.printf("data: %llx\r\n", data);
+
+        irsend.sendMitsubishiAC(data, size);
+    }
+    else
+    {
+        resDoc["error"] = "unknown type.";
+        writeJSONResponse("POST /ir/send", 401, resDoc);
+return;
+    }
+
+    resDoc["success"] = true;
+
+    writeJSONResponse("POST /ir/send", 200, resDoc);
 }
 
 void handleIRDecodeAPI(void)
@@ -157,14 +311,7 @@ void handleIRDecodeAPI(void)
 
     doc["ts"] = now;
 
-    char bodyBuf[1024 * 2];
-    size_t bodySize = serializeJson(doc, bodyBuf, sizeof(bodyBuf));
-    Serial.println(bodyBuf);
-    server.send(200, "application/json", bodyBuf);
-
-    Serial.println("GET /ir/receive 200");
-    pixels.setPixelColor(0, COLOR_NORMAL);
-    pixels.show();
+    writeJSONResponse("GET /ir/receive", 200, doc);
 }
 
 uint32_t latestStatMS = 0;
@@ -184,9 +331,29 @@ void handleMonitorOutput()
     Serial.println(WiFi.localIP());
 }
 
+// true: released, false: pushed
+bool buttonState = false;
+
+void handleButton()
+{
+    bool current = digitalRead(BUTTON_PIN);
+    bool nowReleased = current && !buttonState;
+    buttonState = current;
+    if (!nowReleased)
+        return;
+
+    Serial.println("Button Pushed");
+    pixels.setPixelColor(0, COLOR_SEND);
+
+    irsend.sendSony(0xa90, 12, 2); // 12 bits & 2 repeats
+
+    pixels.setPixelColor(0, COLOR_NORMAL);
+}
+
 void loop()
 {
     // handleIRDecode();
     server.handleClient();
     handleMonitorOutput();
+    handleButton();
 }
