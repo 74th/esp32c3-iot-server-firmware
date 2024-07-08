27fe2d1b475d674642e5e488cd123c634a8b75c1
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2024-07-08 22:32:42 +0900

    support multiple aircon

--- main-firmware/src/ir.cpp
+++ main-firmware/src/ir.cpp
@@ -15,6 +15,60 @@
 #define UINT64_STR_LEN 16
 #define RECEIVING_MAX_WAIT_MS 5000
 
+#define TYPE_SONY "SONY"
+#define TYPE_DAIKIN "DAIKIN"
+#define TYPE_DAIKIN2 "DAIKIN2"
+#define TYPE_DAIKIN128 "DAIKIN128"
+#define TYPE_DAIKIN152 "DAIKIN152"
+#define TYPE_DAIKIN160 "DAIKIN160"
+#define TYPE_DAIKIN176 "DAIKIN176"
+#define TYPE_DAIKIN200 "DAIKIN200"
+#define TYPE_DAIKIN216 "DAIKIN216"
+#define TYPE_DAIKIN312 "DAIKIN312"
+#define TYPE_FUJITSU_AC "FUJITSU_AC"
+#define TYPE_MITSUBISHI_AC "MITSUBISHI_AC"
+#define TYPE_TOSHIBA_AC "TOSHIBA_AC"
+#define TYPE_HITACHI_AC "HITACHI_AC"
+#define TYPE_HITACHI_AC1 "HITACHI_AC1"
+#define TYPE_HITACHI_AC2 "HITACHI_AC2"
+#define TYPE_HITACHI_AC3 "HITACHI_AC3"
+#define TYPE_HITACHI_AC264 "HITACHI_AC264"
+#define TYPE_HITACHI_AC296 "HITACHI_AC296"
+#define TYPE_HITACHI_AC344 "HITACHI_AC344"
+#define TYPE_HITACHI_AC424 "HITACHI_AC424"
+#define TYPE_SANYO_AC "SANYO_AC"
+#define TYPE_SANYO_AC88 "SANYO_AC88"
+#define TYPE_SANYO_AC152 "SANYO_AC152"
+#define TYPE_SHARP_AC "SHARP_AC"
+#define TYPE_SHARP_AC152 "SHARP_AC152"
+
+#define TYPE_NO_SONY 1
+#define TYPE_NO_DAIKIN 2
+#define TYPE_NO_DAIKIN2 3
+#define TYPE_NO_DAIKIN128 4
+#define TYPE_NO_DAIKIN152 5
+#define TYPE_NO_DAIKIN160 6
+#define TYPE_NO_DAIKIN176 7
+#define TYPE_NO_DAIKIN200 8
+#define TYPE_NO_DAIKIN216 9
+#define TYPE_NO_DAIKIN312 10
+#define TYPE_NO_FUJITSU_AC 11
+#define TYPE_NO_MITSUBISHI_AC 12
+#define TYPE_NO_TOSHIBA_AC 13
+#define TYPE_NO_HITACHI_AC 14
+#define TYPE_NO_HITACHI_AC1 15
+#define TYPE_NO_HITACHI_AC2 16
+#define TYPE_NO_HITACHI_AC3 17
+#define TYPE_NO_HITACHI_AC264 18
+#define TYPE_NO_HITACHI_AC296 19
+#define TYPE_NO_HITACHI_AC344 20
+#define TYPE_NO_HITACHI_AC424 21
+#define TYPE_NO_SANYO_AC 22
+#define TYPE_NO_SANYO_AC88 23
+#define TYPE_NO_SANYO_AC152 24
+#define TYPE_NO_SHARP_AC 25
+#define TYPE_NO_SHARP_AC152 26
+
 const uint16_t kCaptureBufferSize = 1024;
 const uint8_t kTimeout = 50;
 const uint8_t kTolerancePercentage = kTolerance;
@@ -106,7 +160,6 @@ void handleIRSendAPI(void)
         return;
     }
 
-    uint64_t data[32];
     const char *hexData = reqDoc["hex"];
     if (hexData == NULL || strlen(hexData) == 0)
     {
@@ -125,51 +178,229 @@ void handleIRSendAPI(void)
         return;
     }
 
-    Serial.printf("type: %s, hex: %s\r\n", type, hexData);
-
-    if (strcmp(type, "SONY") == 0)
+    uint16_t typeNo;
+    if (strcmp(type, TYPE_SONY) == 0)
     {
-        uint64_t data = hexStringToUint64(hexData);
-        Serial.printf("data: %llx\r\n", data);
-        irsend.sendSony(data, 12, 2);
+        typeNo = TYPE_NO_SONY;
     }
-    else if (strcmp(type, "MITSUBISHI_AC") == 0)
+    else if (strcmp(type, TYPE_DAIKIN) == 0)
     {
-        if (strlen(hexData) > 32 * UINT64_STR_LEN)
-        {
-            resDoc["error"] = "hex is too long.";
-            writeJSONResponse("POST /ir/send", 401, resDoc);
-            return;
-        }
-
-        unsigned char data[64];
-        size_t size;
-        hexStringToByteArray(hexData, data, &size);
-        Serial.printf("data: %llx\r\n", data);
-
-        irsend.sendMitsubishiAC(data, size);
+        typeNo = TYPE_NO_DAIKIN;
     }
-    else if (strcmp(type, "SHARP_AC") == 0)
+    else if (strcmp(type, TYPE_DAIKIN2) == 0)
     {
-        if (strlen(hexData) > 32 * UINT64_STR_LEN)
-        {
-            resDoc["error"] = "hex is too long.";
-            writeJSONResponse("POST /ir/send", 401, resDoc);
-            return;
-        }
-
-        unsigned char data[64];
-        size_t size;
-        hexStringToByteArray(hexData, data, &size);
-        Serial.printf("data: %llx\r\n", data);
-
-        irsend.sendSharpAc(data, size);
+        typeNo = TYPE_NO_DAIKIN2;
+    }
+    else if (strcmp(type, TYPE_DAIKIN128) == 0)
+    {
+        typeNo = TYPE_NO_DAIKIN128;
+    }
+    else if (strcmp(type, TYPE_DAIKIN152) == 0)
+    {
+        typeNo = TYPE_NO_DAIKIN152;
+    }
+    else if (strcmp(type, TYPE_DAIKIN160) == 0)
+    {
+        typeNo = TYPE_NO_DAIKIN160;
+    }
+    else if (strcmp(type, TYPE_DAIKIN176) == 0)
+    {
+        typeNo = TYPE_NO_DAIKIN176;
+    }
+    else if (strcmp(type, TYPE_DAIKIN200) == 0)
+    {
+        typeNo = TYPE_NO_DAIKIN200;
+    }
+    else if (strcmp(type, TYPE_DAIKIN216) == 0)
+    {
+        typeNo = TYPE_NO_DAIKIN216;
+    }
+    else if (strcmp(type, TYPE_DAIKIN312) == 0)
+    {
+        typeNo = TYPE_NO_DAIKIN312;
+    }
+    else if (strcmp(type, TYPE_FUJITSU_AC) == 0)
+    {
+        typeNo = TYPE_NO_FUJITSU_AC;
+    }
+    else if (strcmp(type, TYPE_MITSUBISHI_AC) == 0)
+    {
+        typeNo = TYPE_NO_MITSUBISHI_AC;
+    }
+    else if (strcmp(type, TYPE_TOSHIBA_AC) == 0)
+    {
+        typeNo = TYPE_NO_TOSHIBA_AC;
+    }
+    else if (strcmp(type, TYPE_HITACHI_AC) == 0)
+    {
+        typeNo = TYPE_NO_HITACHI_AC;
+    }
+    else if (strcmp(type, TYPE_HITACHI_AC1) == 0)
+    {
+        typeNo = TYPE_NO_HITACHI_AC1;
+    }
+    else if (strcmp(type, TYPE_HITACHI_AC2) == 0)
+    {
+        typeNo = TYPE_NO_HITACHI_AC2;
+    }
+    else if (strcmp(type, TYPE_HITACHI_AC3) == 0)
+    {
+        typeNo = TYPE_NO_HITACHI_AC3;
+    }
+    else if (strcmp(type, TYPE_HITACHI_AC264) == 0)
+    {
+        typeNo = TYPE_NO_HITACHI_AC264;
+    }
+    else if (strcmp(type, TYPE_HITACHI_AC296) == 0)
+    {
+        typeNo = TYPE_NO_HITACHI_AC296;
+    }
+    else if (strcmp(type, TYPE_HITACHI_AC344) == 0)
+    {
+        typeNo = TYPE_NO_HITACHI_AC344;
+    }
+    else if (strcmp(type, TYPE_HITACHI_AC424) == 0)
+    {
+        typeNo = TYPE_NO_HITACHI_AC424;
+    }
+    else if (strcmp(type, TYPE_SANYO_AC) == 0)
+    {
+        typeNo = TYPE_NO_SANYO_AC;
+    }
+    else if (strcmp(type, TYPE_SHARP_AC) == 0)
+    {
+        typeNo = TYPE_NO_SHARP_AC;
+    }
+    else if (strcmp(type, TYPE_SHARP_AC152) == 0)
+    {
+        typeNo = TYPE_NO_SHARP_AC152;
     }
     else
     {
         resDoc["error"] = "unknown type.";
         writeJSONResponse("POST /ir/send", 401, resDoc);
         return;
+    }
+
+    Serial.printf("type: %s, hex: %s\r\n", type, hexData);
+
+    unsigned char data[64];
+    size_t size;
+
+    switch (typeNo)
+    {
+    case TYPE_NO_SONY:
+        data[0] = hexStringToUint64(hexData);
+        Serial.printf("data: %llx\r\n", data);
+        break;
+    case TYPE_NO_DAIKIN:
+    case TYPE_NO_DAIKIN2:
+    case TYPE_NO_DAIKIN128:
+    case TYPE_NO_DAIKIN152:
+    case TYPE_NO_DAIKIN160:
+    case TYPE_NO_DAIKIN176:
+    case TYPE_NO_DAIKIN200:
+    case TYPE_NO_DAIKIN216:
+    case TYPE_NO_DAIKIN312:
+    case TYPE_NO_FUJITSU_AC:
+    case TYPE_NO_MITSUBISHI_AC:
+    case TYPE_NO_TOSHIBA_AC:
+    case TYPE_NO_HITACHI_AC:
+    case TYPE_NO_HITACHI_AC1:
+    case TYPE_NO_HITACHI_AC2:
+    case TYPE_NO_HITACHI_AC3:
+    case TYPE_NO_HITACHI_AC264:
+    case TYPE_NO_HITACHI_AC296:
+    case TYPE_NO_HITACHI_AC344:
+    case TYPE_NO_HITACHI_AC424:
+    case TYPE_NO_SANYO_AC:
+    case TYPE_NO_SHARP_AC:
+    case TYPE_NO_SHARP_AC152:
+        hexStringToByteArray(hexData, data, &size);
+        Serial.printf("data: %llx\r\n", data);
+        break;
+    }
+
+    switch (typeNo)
+    {
+    case TYPE_NO_SONY:
+        irsend.sendSony(data[0], 12, 2);
+        break;
+    case TYPE_NO_DAIKIN:
+        irsend.sendDaikin(data, size);
+        break;
+    case TYPE_NO_DAIKIN2:
+        irsend.sendDaikin2(data, size);
+        break;
+    case TYPE_NO_DAIKIN128:
+        irsend.sendDaikin128(data, size);
+        break;
+    case TYPE_NO_DAIKIN152:
+        irsend.sendDaikin152(data, size);
+        break;
+    case TYPE_NO_DAIKIN160:
+        irsend.sendDaikin160(data, size);
+        break;
+    case TYPE_NO_DAIKIN176:
+        irsend.sendDaikin176(data, size);
+        break;
+    case TYPE_NO_DAIKIN200:
+        irsend.sendDaikin200(data, size);
+        break;
+    case TYPE_NO_DAIKIN216:
+        irsend.sendDaikin216(data, size);
+        break;
+    case TYPE_NO_DAIKIN312:
+        irsend.sendDaikin312(data, size);
+        break;
+    case TYPE_NO_FUJITSU_AC:
+        irsend.sendFujitsuAC(data, size);
+        break;
+    case TYPE_NO_MITSUBISHI_AC:
+        irsend.sendMitsubishiAC(data, size);
+        break;
+    case TYPE_NO_TOSHIBA_AC:
+        irsend.sendToshibaAC(data, size);
+        break;
+    case TYPE_NO_HITACHI_AC:
+        irsend.sendHitachiAC(data, size);
+        break;
+    case TYPE_NO_HITACHI_AC1:
+        irsend.sendHitachiAC1(data, size);
+        break;
+    case TYPE_NO_HITACHI_AC2:
+        irsend.sendHitachiAC2(data, size);
+        break;
+    case TYPE_NO_HITACHI_AC3:
+        irsend.sendHitachiAc3(data, size);
+        break;
+    case TYPE_NO_HITACHI_AC264:
+        irsend.sendHitachiAc264(data, size);
+        break;
+    case TYPE_NO_HITACHI_AC296:
+        irsend.sendHitachiAc296(data, size);
+        break;
+    case TYPE_NO_HITACHI_AC344:
+        irsend.sendHitachiAc344(data, size);
+        break;
+    case TYPE_NO_HITACHI_AC424:
+        irsend.sendHitachiAc424(data, size);
+        break;
+    case TYPE_NO_SANYO_AC:
+        irsend.sendSanyoAc(data, size);
+        break;
+    case TYPE_NO_SANYO_AC88:
+        irsend.sendSanyoAc88(data, size);
+        break;
+    case TYPE_NO_SANYO_AC152:
+        irsend.sendSanyoAc152(data, size);
+        break;
+    case TYPE_NO_SHARP_AC:
+        irsend.sendSharpAc(data, size);
+        break;
+    case TYPE_NO_SHARP_AC152:
+        irsend.sendSharpAc(data, size);
+        break;
     }
 
     resDoc["success"] = true;
